e79f9a69c525dfdbe44a240cc1d92d595396267d
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-07-03 15:40:00 +0500

    QR Code Generator

--- Generate_Qrcode.py
+++ Generate_Qrcode.py
@@ -1,4 +1,16 @@
 import qrcode
-qr_img = qrcode.make("www.linkedin.com/in/kelash")
 
-qr_img.save("qr-img.jpg")
+# Create QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+data = "www.linkedin.com/in/kelash"
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image of the QR code
+img = qr.make_image(fill='black', back_color='white')
+img.save("example_qr.png")
