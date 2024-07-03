24e1eae399ec603c8ac471e4417bbe137ad378ec
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-07-03 15:12:19 +0500

    Updated

--- Generate_Qrcode.py
+++ Generate_Qrcode.py
@@ -1,1 +1,16 @@
-print("Hi")
+import qrcode
+
+# Create QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+data = "https://www.example.com"
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image of the QR code
+img = qr.make_image(fill='black', back_color='white')
+img.save("example_qr.png")
