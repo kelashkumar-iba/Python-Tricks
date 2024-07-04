62733b6af0cfc2b48db3066d711d958a78c3ec90
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-07-04 18:49:49 +0500

    Countdown Timer

---
+++ Countdown_Timer.py
@@ -0,0 +1,13 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = f"{mins:02d}:{secs:02d}"
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("Time's up!")
+
+countdown_timer(10)
