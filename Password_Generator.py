a28d3239be6ffa246996ea5f4b08920b2eb2570b
kelashkumar-iba <kelashkumar.bscsf22@iba-suk.edu.pk>
2024-07-04 18:29:07 +0500

    Password Generator

--- Password_Generator.py
+++ Password_Generator.py
@@ -5,3 +5,5 @@ def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choice(characters) for _ in range(length))
     return password
+
+print(f"Generated Password: {generate_password(16)}")
